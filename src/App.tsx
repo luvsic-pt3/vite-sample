1ab310a978f515018010f004df7012cdf9249d55
上森香 <uemori.kaoru@adways.net>
2022-08-19 22:34:13 +0900

    :sparkles: react-hook-form導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Form from "./components/Form/form";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,28 +16,7 @@ function App() {
             count is: {count}
           </button>
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <Form />
       </header>
     </div>
   )
